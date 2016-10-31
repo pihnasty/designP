440f0ed218c5e8eb8b950e21b922a88d4ccde3c9
Oleh Oihnastyi H <innov@bk.ru>
2016-10-31 06:54:19 +0200

    String

--- src/main/java/tests/testString.java
+++ src/main/java/tests/testString.java
@@ -9,8 +9,41 @@ public class testString {
 
         s=null;
 
-        if (  s.isEmpty() || s == null)
-        System.out.println("Yes");
+        if (  s == null  ||  s.isEmpty())  System.out.println("Yes");
+
+
+                String queryHistory =  " select query_text, query_count\n" +
+                        "                            from\n" +
+                        "                            (\n" +
+                        "                            select query_text, count(*) query_count\n" +
+                        "                            from public.queries_history\n" +
+                        "                            where db = {0}\n" +                          // {0}     'postgres'
+                        "                             and\n" +
+                        "                             query_text <>''\n" +
+                        "                             and lower(query_text) like '%{1}%'    " +                     // '%green_dwh%'      '%{1}%'
+
+                        "                            group by query_text\n" +
+                        "                            ) q\n" +
+                        "                            order by query_count desc;";
+
+
+
+
+
+
+        System.out.println(queryHistory.replace("{0}", "'postgres'").replace("{1}", "green_dwh"));
+
+       String sLjader = "DbLoaderNоDataToProcessingException";
+
+
+        char[] chArray = sLjader.toCharArray();
+
+
+        for (int i=0; i<chArray.length; i++) {
+            System.out.println("code="+(int)chArray[i]+"\tsumbol="+chArray[i]);
+        }
+
+
 
     }
 }
