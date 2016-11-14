474f86994d0db97d6f05ac14def2fd72f903d205
Oleh Oihnastyi H <innov@bk.ru>
2016-11-14 11:26:36 +0200

    Adapter

---
+++ src/main/java/pattern/AdapterExample.java
@@ -0,0 +1,66 @@
+package pattern;
+
+interface Chief {
+    public Object makeBreakfast();
+    public Object makeDinner();
+    public Object makeSupper();
+}
+
+
+// Файл Plumber.java
+
+class Plumber {
+
+    public Object getPipe(){
+        System.out.println("getPipe()");
+        return new Object();
+    }
+
+    public Object getKey(){
+        System.out.println("getKey()");
+        return new Object();
+    }
+
+    public Object getScrewDriver(){
+        System.out.println("getScrewDriver()");
+        return new Object();
+    }
+
+}
+
+
+// Файл ChiefAdapter.java
+
+class ChiefAdapter implements Chief{
+
+    private Plumber plumber = new Plumber();
+
+    @Override
+    public Object makeBreakfast() {
+        return plumber.getKey();
+    }
+
+    @Override
+    public Object makeDinner() {
+        return plumber.getScrewDriver();
+    }
+
+    @Override
+    public Object makeSupper() {
+        return plumber.getPipe();
+    }
+
+}
+
+
+// Файл Client.java
+
+public class AdapterExample {
+
+    public static void main (String [] args){
+        Chief chief = new ChiefAdapter();
+
+        Object key = chief.makeDinner();
+    }
+
+}
