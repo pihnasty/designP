7131329e4532d834cacf9233fd65fa77b003d364
Oleh Oihnastyi H <innov@bk.ru>
2017-01-27 09:08:32 +0200

    SimpleThread2

---
+++ src/main/java/brus/TIJ4/concurrency/_915_join/NaiveExceptionHandling.java
@@ -0,0 +1,17 @@
+package brus.TIJ4.concurrency._915_join;//: concurrency/NaiveExceptionHandling.java
+// {ThrowsException}
+
+import java.util.concurrent.*;
+
+public class NaiveExceptionHandling {
+  public static void main(String[] args) {
+    try {
+      ExecutorService exec =
+        Executors.newCachedThreadPool();
+      exec.execute(new ExceptionThread());
+    } catch(RuntimeException ue) {
+      // This statement will NOT execute!
+      System.out.println("Exception has been handled!");
+    }
+  }
+} ///:~
