f5ce154d2da367c29f0104ea3aa41085c0b6360a
Oleh Pihnasty <innov@bk.ru>
2017-03-20 13:32:48 +0200

    SQLException

---
+++ src/main/java/tests/enumTest/EnumTest.java
@@ -0,0 +1,35 @@
+package tests.enumTest;
+
+/**
+ * Created by Pihnastyi.O on 3/13/2017.
+ */
+public class EnumTest {
+    public static void main(String[] args) {
+
+        System.out.println(ServerMessages.INVALID_SSL_HANDSHAKE.setMessage("dfdf"));
+        System.out.println(ServerMessages.INVALID_SSL_HANDSHAKE.getMessage());
+
+
+        ServerMessages enumS2 = ServerMessages.SUCCESS;
+        System.out.println(enumS2.getMessage());
+
+    }
+
+    enum ServerMessages {
+        SUCCESS, INVALID_CREDENTIALS, INVALID_CONNECTION_PROPS,
+        INVALID_SSL_HANDSHAKE;
+
+        public ServerMessages setMessage(String s) {
+            sqlExceptionMessage = s;
+            return this;
+        }
+
+        public String getMessage() {
+            return sqlExceptionMessage;
+        }
+
+        private String sqlExceptionMessage="2";
+
+    }
+}
+
