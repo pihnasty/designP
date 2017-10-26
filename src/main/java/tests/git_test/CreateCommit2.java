f6ad15b11135ee8cf2082f336f074e403f51023b
POM <innov@bk.ru>
2017-10-26 12:18:44 +0300

    Проверяю  SOFT jckt reset

--- src/main/java/tests/git_test/CreateCommit2.java
+++ src/main/java/tests/git_test/CreateCommit2.java
@@ -10,9 +10,11 @@ public class CreateCommit2 {
 * This will reset the current branch  head  to the selected commit, and update the working tree and the index according to the selected mode
 
 soft:  files won't change, differences will be staged for commit
-mixed:    files won't change, differences won't be staged
-hard:      Files will be reverted to the state of the selected commit. Warning: any local changes will be lost
-keep:     Files will be reverted to the state of the selected commit, but local changes will be kept intact
+
+
+Проверяю  SOFT
+
+
 *
 * */
 
