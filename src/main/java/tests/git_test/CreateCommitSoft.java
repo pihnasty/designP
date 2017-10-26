96755e499df1a8c8d4c07436602b3f15cd327ca6
POM <innov@bk.ru>
2017-10-26 13:17:01 +0300

    Reset current branch to Here

--- src/main/java/tests/git_test/CreateCommitSoft.java
+++ src/main/java/tests/git_test/CreateCommitSoft.java
@@ -4,4 +4,19 @@ package tests.git_test;
  * Created by Pihnastyi.O on 10/26/2017.
  */
 public class CreateCommitSoft {
+
+    /*
+    *
+    * self
+    *
+    * */
+
+    /*
+
+    to two
+
+    */
+
+
+
 }
