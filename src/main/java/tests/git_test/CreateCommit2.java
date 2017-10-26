2c0ce4ed27625e4a344c55bbeefc83beb54ae216
POM <innov@bk.ru>
2017-10-26 12:07:16 +0300

    Мне надо изменить текст сообщения

--- src/main/java/tests/git_test/CreateCommit2.java
+++ src/main/java/tests/git_test/CreateCommit2.java
@@ -4,4 +4,17 @@ package tests.git_test;
  * Created by Pihnastyi.O on 10/24/2017.
  */
 public class CreateCommit2 {
+
+/*
+*
+* This will reset the current branch  head  to the selected commit, and update the working tree and the index according to the selected mode
+
+soft:  files won't change, differences will be staged for commit
+mixed:    files won't change, differences won't be staged
+hard:      Files will be reverted to the state of the selected commit. Warning: any local changes will be lost
+keep:     Files will be reverted to the state of the selected commit, but local changes will be kept intact
+*
+* */
+
+
 }
