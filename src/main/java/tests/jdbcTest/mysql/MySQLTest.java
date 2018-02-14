35b0b109e1ca1607072efd806d7edd2e4e376c63
POM <innov@bk.ru>
2018-02-14 23:41:02 +0200

    Added MySQL

--- src/main/java/tests/jdbcTest/mysql/MySQLTest.java
+++ src/main/java/tests/jdbcTest/mysql/MySQLTest.java
@@ -25,7 +25,7 @@ public class MySQLTest {
         try {
             connection = DriverManager
                     //      .getConnection("jdbc:mysql://localhost:3306/library", "root", "root");
-                    //     .getConnection("jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11219539", "sql11219539", "h1eSJdY9Y6");
+                   //     .getConnection("jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11219539", "sql11219539", "h1eSJdY9Y6");
                     .getConnection("jdbc:mysql://www.db4free.net:3306/productionmodel", "production", "production");
 
 
@@ -36,9 +36,12 @@ public class MySQLTest {
             }
 
             Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery("CREATE TABLE productionmodel.machine (id INT(10),name VARCHAR(64),description VARCHAR(256))");
 
-
+ //           st.executeUpdate("INSERT INTO productionmodel.machine VALUES ( 3,'KD 2323_03',   'Press_03')");
+            ResultSet rs = st.executeQuery("select * from productionmodel.machine ");
+    //        ResultSet rs = st.executeQuery("select * from sql11219539.mashine ");
+     //       st.executeUpdate("CREATE TABLE machine (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,name VARCHAR(32),description VARCHAR(256))");
+//CREATE TABLE employees (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, lastname VARCHAR(20), firstname VARCHAR(20), phone VARCHAR(20),  dateofbirth DATE)
             while (rs.next()) {
                 System.out.println("text: " + rs.getString(2));
             }
