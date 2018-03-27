79be77c1ad8bb2b1a57b5e401541209e20844b00
Oleg Pihnastyi <Pihnastyi.o@dbbest.com>
2018-03-27 19:20:32 +0300

    StringParser variant 01

--- src/main/java/tests/core/StringTest/StringParser.java
+++ src/main/java/tests/core/StringTest/StringParser.java
@@ -1,32 +1,29 @@
 package tests.core.StringTest;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class StringParser {
     public static void main(String[] args) {
         StringParser stringParser = new StringParser();
-        String options = " {             host    =   ec2-52-33-224-137.us-west-2.compute.amazonaws.com,port     =      5432       ,dbname     =    test_pg_mysql              }";
+        String options = "    {             host    =   ec2-52-33-224-137.us-west-2.compute.amazonaws.com,port     =      5432       ,dbname     =    test_pg_mysql              }";
 
+     options=null;
 
-        stringParser.parseOptions(options, ",\n  ");
+        System.out.println(stringParser.parseOptions(options, ",","        "));
 
 
     }
 
-    private void parseOptions(String options, String delimiter) {
+    private String parseOptions(String options, String delimiter, String indention) {
 
+        if (Objects.isNull(options) || options.trim().length() < 2) return options;
         options = options.trim();
-
-        String s2 = Arrays.asList(options.trim().substring(1, options.length() - 1)
-                .split(","))
-                .stream().map(e -> e.split("="))
-                .map(strings -> strings[0].trim() + "=" + singleQuote(strings[1].trim())).collect(Collectors.joining(delimiter, "( ", " )"));
-
-
-        System.out.println(s2);
-
-
+        return Arrays.stream(options.trim().substring(1, options.length() - 1).split(","))
+                .map(e -> e.split("="))
+                .map(strings -> "\n" + indention + strings[0].trim() + " " + singleQuote(strings[1].trim()))
+                .collect(Collectors.joining(delimiter, "(", " )"));
     }
 
     private static String singleQuote(String s) {
---
+++ src/main/java/tests/core/constructors/A.java
@@ -0,0 +1,10 @@
+package tests.core.constructors;
+
+public class A {
+    public static void main(String[] args) {
+        B b = new B();
+    }
+}
+
+
+//      ----------------------
