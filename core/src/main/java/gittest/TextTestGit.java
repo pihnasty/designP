9158a2bd1012c02cc94fc2b707750f8cb5423c7e
POM <innov@bk.ru>
2018-09-17 11:49:43 +0300

    It is glue commit

--- core/src/main/java/gittest/TextTestGit.java
+++ core/src/main/java/gittest/TextTestGit.java
@@ -3,6 +3,6 @@ package gittest;
 public class TextTestGit {
     /**
      *  2018_09_17_11_18_     It is main
-     *  2018_09_17_11_21_     It is main before create glue
+     *  2018_09_17_11_21_     It is main before create glue  main
      */
 }
