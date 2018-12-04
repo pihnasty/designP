27ce99519d55ce97af72cea9740783420107f1b6
POM <innov@bk.ru>
2018-12-04 22:37:42 +0200

    bi2

--- dbm/src/main/java/com/dbmsys/csvapi/io/write/AbstractWriterP.java
+++ dbm/src/main/java/com/dbmsys/csvapi/io/write/AbstractWriterP.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
-
+//
 public abstract class AbstractWriterP implements WriterP {
     private String  path;
     private String fileName;
