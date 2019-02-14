43c18a7125cab0fc69284147c1c8e68273ec8b8b
POM <pom7@bk.ru>
2019-02-14 13:06:58 +0200

    pdf

---
+++ core/src/main/java/observable/ObservableP.java
@@ -0,0 +1,13 @@
+package observable;
+
+
+import java.util.Observable;
+import java.util.Timer;
+
+public class ObservableP extends Observable {
+
+
+Timer timer;
+
+
+}
---
+++ core/src/main/java/observable/ObserverP.java
@@ -0,0 +1,14 @@
+package observable;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ObserverP implements Observer {
+    @Override
+    public void update(Observable o, Object arg) {
+
+  //      o.setChanged();
+    }
+
+
+}
