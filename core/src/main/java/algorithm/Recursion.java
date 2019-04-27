753108b1aa7f26b7efa72294e97b5f8dfb77f45b
POM <pom7@bk.ru>
2019-04-27 22:41:31 +0300

    recursion

--- core/src/main/java/algorithm/Recursion.java
+++ core/src/main/java/algorithm/Recursion.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class Recursion {
     public static void main(String[] args) {
-        int n = 2;
-        int N = 40;
-        List<List<Long>> sample = new ArrayList<>();
+        int n = 1;
+        int N = 10;
+        List<List<Long>> reverseSample = new ArrayList<>();
 
         Recursion recursion = new Recursion();
 
@@ -16,13 +16,21 @@ public class Recursion {
 //                factor -> System.out.println(factor)
 //        );
 
-        recursion.forDynamyc (N, n, 0, sample, null);
+        recursion.forDynamyc (N, n, 0, reverseSample, null);
 
         long finishTime = System.currentTimeMillis();
 
+        System.out.printf("  %d   %d   %d   %d", N,n,reverseSample.size(),(finishTime-startTime)/1000);
+        System.out.println();
+
+        List<List<Long>> sample  =  recursion.reverse (N, reverseSample);
+
         System.out.printf("  %d   %d   %d   %d", N,n,sample.size(),(finishTime-startTime)/1000);
+        System.out.println();
 
-
+        sample.forEach(
+                row-> System.out.println(row)
+        );
 
     }
 
@@ -30,6 +38,7 @@ public class Recursion {
     public void forDynamyc(int N, int n, int k, List<List<Long>> sample, List<Long> row) {
         for (long i = k; i < N; i++) {
             if (k == 0) {
+                System.out.println(i);
                 row = new ArrayList<>();
                 for (int i2 = 0; i2 < n; i2++) {
                     row.add((long) -1);
@@ -37,17 +46,34 @@ public class Recursion {
             }
             if (k == 0 || i > row.get(k - 1)) {
                 row.set(k, i);
-
                 if (k < n - 1) {
                     forDynamyc(N, n, k + 1, sample, row);
                 } else {
-                    sample.add(row);
-                    row = new ArrayList<>();
-                    for (int i2 = 0; i2 < n; i2++) {
-                        row.add(sample.get(sample.size() - 1).get(i2));
+                    List<Long> cloneRow = new ArrayList<>();
+                    for (int iClone = 0; iClone < n; iClone++) {
+                        cloneRow.add(row.get(iClone));
                     }
+                    sample.add(cloneRow);
                 }
             }
         }
+    }
+
+    public List<List<Long>> reverse (int N, List<List<Long>> reverseFactorNumbersList) {
+        List<List<Long>>  factorNumbersList = new ArrayList<>();
+        List<Long> fullFactorNumbersRow = new ArrayList<>();
+        for (int i=0; i<N; i++) {
+            fullFactorNumbersRow .add((long) i);
+        }
+        reverseFactorNumbersList.forEach(
+                reverseFactorNumbersRow -> {
+                    List<Long> factorNumbersRow = (List<Long>) ((ArrayList<Long>) fullFactorNumbersRow).clone();
+                    factorNumbersRow.removeAll(reverseFactorNumbersRow);
+                    factorNumbersList.add(factorNumbersRow);
+                }
+        );
+
+        return factorNumbersList;
+
     }
 }
