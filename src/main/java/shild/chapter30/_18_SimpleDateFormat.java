947e3e6e2454c33df68f327a9c51633f110604c8
POM <pom7@bk.ru>
2020-03-31 14:03:18 +0300

    addMath

--- src/main/java/shild/chapter30/_18_SimpleDateFormat.java
+++ src/main/java/shild/chapter30/_18_SimpleDateFormat.java
@@ -1,18 +1,7 @@
 package shild.chapter30;
 
-// Demonstrate SimpleDateFormat.
-import java.text.*;
-import java.util.*;
+
 
 public class _18_SimpleDateFormat {
-    public static void main(String args[]) {
-        Date date = new Date();
-        SimpleDateFormat sdf;
-        sdf = new SimpleDateFormat("hh:mm:ss");
-        System.out.println(sdf.format(date));
-        sdf = new SimpleDateFormat("dd MMM yyyy hh:mm:ss zzz");
-        System.out.println(sdf.format(date));
-        sdf = new SimpleDateFormat("E MMM dd yyyy");
-        System.out.println(sdf.format(date));
-    }
+
 }
